2d4b109fed92c819295b4d24840dacc9264da08f
Smisklas <niclas_v.johansson@maxiv.lu.se>
2019-02-19 17:07:33 +0100

    Updated the package, which can now be isntalled directly into python via the setup.py
    	modified:   README.md
    	modified:   setup.py
    	renamed:    species-xps/__init__.py -> species_xps/__init__.py
    	renamed:    species-xps/core.py -> species_xps/core.py

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# species-xps
-library for handling XPS data from the SPECIES beamline at the MAX IV Laboratory
+
+===================
+   species-xps
+===================
+
+A library for handling XPS data from the species beamline at the MAX IV Laboratory.
+
+*** UNDER DEVELOPEMENT***
+
+Instalation instruction:
+
+clone this repositry.
+
+enter the following commands:
+cd PATH_TO_REPO/species-xps
+python setup.py build
+pytohn setup.py install
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ setup(
     version='0.1.0',
     author='N. Johansson',
     author_email='niclas_v.johansson@maxiv.lu.se',
-    packages=['species-xps'],
+    packages=['species_xps'],
     url='http://github.com/Smisklas/species-xps/',
     license='LICENSE.txt',
     description='Library for the processing of XPS data',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "datetime",
-        "numpy",
-        're',
-    ],
+    long_description=open('README.md').read(),
+    #install_requires=[
+     #   "datetime",
+    #    "numpy",
+    #    're',
+   # ],
 )
